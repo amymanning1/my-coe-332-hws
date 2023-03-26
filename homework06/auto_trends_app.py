d163f9233829cc2ca185cc65f3b58ee2cb22df60
Amy Manning <amycurly.manning@gmail.com>
2023-03-26 04:47:03 +0000

    some construction

--- homework06/auto_trends_app.py
+++ homework06/auto_trends_app.py
@@ -1,6 +1,14 @@
 from flask import Flask
-import redis
+import redis, csv
+from csv import DictReader
 app = Flask(__name__)
+
+filename = 'auto-trends-manufacturer.csv'
+with open(filename, 'r') as data:
+    dict_reader = csv.DictReader(f) 
+    list_of_dict = list(dict_reader)
+    print(list_of_dict)
+
 
 def get_redis_client():
     return redis.Redis(host='127.0.0.1', port=6379, db=0, decode_responses=True)
