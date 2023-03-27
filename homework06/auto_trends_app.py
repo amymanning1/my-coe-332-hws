e195d9faea6f4301432ddc4c507618cc75ee13bd
Amy Manning <amycurly.manning@gmail.com>
2023-03-27 04:09:09 +0000

    Completed coding app

--- homework06/auto_trends_app.py
+++ homework06/auto_trends_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-import redis, csv
+import redis, csv, json
 from csv import DictReader
 app = Flask(__name__)
 
@@ -10,11 +10,22 @@ with open(filename, 'r') as f:
 
 
 def get_redis_client():
+    """
+    This function initializes the redis client.
+    Args: none
+    Returns: redis client: connects redis to flask and docker
+    """
     return redis.Redis(host='redis-db', port=6379, db=0, decode_responses=True)
 rd = get_redis_client()
 
 @app.route('/data', methods=['GET', 'POST', 'DELETE'])
 def handle_data():
+    """
+    This function either loads or deletes the data from redis, or pulls and displays it to the user.
+    Args: none
+    Returns: str: message on status of action
+             output_list (list): list of the entire data set
+    """
     if request.method == 'POST':
         for item in list_of_dict:
             key = f"{item['Manufacturer']}:{item['Model Year']}:{item['Vehicle Type']}"
@@ -34,6 +45,47 @@ def handle_data():
     else:
         return 'wrong method\n'
 
+@app.route('/years', methods=['GET'])
+def get_years():
+    """
+    This function returns a list of years used in the dataset.
+    Args: none
+    Returns: yr_list (list): a list of all years data was collected by the EPA
+    """
+    yr_list=[]
+    for item in list_of_dict:
+            key = f"{item['Manufacturer']}:{item['Model Year']}:{item['Vehicle Type']}"
+            yr = rd.hget(key,'Model Year')
+            yr_list.append(yr)
+    yr_set = set(yr_list) # eliminating duplicate years
+    yr_list = list(yr_set)
+    return yr_list
+
+@app.route('/years/<year>',methods=['GET'])
+def year_data(year):
+    """
+    This function takes in a user specified year and returns all the data from that year
+    Args: year (int): year that the user wants to pull data from
+    Returns: yr_data (list): list of all the data from that year
+    """
+    yr_data=[]
+    if list_of_dict == None:
+        return []
+        exit()
+    yr_list=[]
+    for item in list_of_dict:
+            key = f"{item['Manufacturer']}:{item['Model Year']}:{item['Vehicle Type']}"
+            yr = rd.hget(key,'Model Year')
+            yr_list.append(yr)
+    yr_set = set(yr_list) # eliminating duplicate years
+    yr_list = list(yr_set)
+    if year in yr_list:
+        for item in list_of_dict:
+            key = f"{item['Manufacturer']}:{item['Model Year']}:{item['Vehicle Type']}"
+            yr = rd.hget(key,'Model Year')
+            if yr == str(year):
+                yr_data.append(item)
+        return yr_data
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
